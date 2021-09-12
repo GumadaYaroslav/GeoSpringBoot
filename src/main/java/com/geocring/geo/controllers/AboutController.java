a0e0260a7858742fa92da8224725ae248dd6627a
Free Milissa <fmilissa@ge-h4.kzn.21-school.ru>
2021-09-12 18:47:54 +0300

    about and home page fix

---
+++ src/main/java/com/geocring/geo/controllers/AboutController.java
@@ -0,0 +1,15 @@
+package com.geocring.geo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+    @GetMapping("/about")
+    public String aboutMain(Model model)
+    {
+        model.addAttribute("title", "Страница о нас");
+        return ("about");
+    }
+}
